400b5e170dab29eff60a262302774a87578732a4
Tarzenyt <107318413+Tarzenyt@users.noreply.github.com>
2022-06-30 22:02:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -41,23 +41,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['761192103'] //ur owner number
-global.ownername = "Udavin Wijesundara"
-global.ytname = "YT: UW TECH SHOW" //ur yt chanel name
-global.socialm = "Insta: udavin_wijesundara" //ur github or insta name
-global.location = "Sri-Lanka" //ur location
+global.owner = ['917994172018'] //ur owner number
+global.ownername = "ALTHAF SIR🦋"
+global.ytname = "YT: unknown yt" //ur yt chanel name
+global.socialm = "Insta: "__althaf_x" //ur github or insta name
+global.location = "kerala" //ur location
 
 //bot bombdy
-global.botname = "White King WhatsApp Bot" //ur bot name
+global.botname = "ALTHAF SIR WHATS APP BOT🦋" //ur bot name
 global.websitex = "https://www.youtube.com/channel/UC8CNAgNIXy1O2vLBBSN7tcA" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
-global.author = "Udavin Wijesundara"
+global.packname = "ALTHAF SIR🦋\n\n\n\n\n\n\n🦄🦄🦄 "
+global.author = "ALTHAF SIR🦋"
 global.themeemoji = "🐶"
 global.reactmoji = "😘"
-global.ownertag = ['761192103'] //ur owner tag
-global.ownernummenu = ['761192103'] //ur owner number in menu and all
-global.watermark = "White King WhatsApp Bot." //ur watermark
+global.ownertag = ['917994172018'] //ur owner tag
+global.ownernummenu = ['917994172018'] //ur owner number in menu and all
+global.watermark = "ALTHAF SIR🦋WhatsApp Bot." //ur watermark
 global.botscript = "https://www.youtube.com/watch?v=vXBO1zJbGJ4"
 global.linkz1 = "https://chat.whatsapp.com/Ivsw8fEw5lW0n0qKEDHfUX"
 global.linkz2 = "https://chat.whatsapp.com/Jub3moTE7jaBERo8DqWXC1"
